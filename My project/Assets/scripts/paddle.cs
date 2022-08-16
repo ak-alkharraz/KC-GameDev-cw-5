5192f28134bc66570b7bd66c5201fbce596b0adf
ak-alkharraz <109977574+ak-alkharraz@users.noreply.github.com>
2022-08-17 00:07:41 +0300

    cw5
    hi guys

---
+++ My project/Assets/scripts/paddle.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paddle : MonoBehaviour
+{
+    Rigidbody2D RB;
+    float PlayerInput;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RB = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+    PlayerInput = Input.GetAxis("Horizontal");
+    RB.velocity = new Vector2(PlayerInput * speed, 0);
+    }
+}
